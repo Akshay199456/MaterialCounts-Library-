af44739a925ce4aa8b292a3d05516918192f081e
Akshay199456 <akshay.kum94@gmail.com>
2020-01-13 12:26:34 -0600

    Wrote initial code

---
+++ code.py
@@ -0,0 +1,11 @@
+# Keep count of each kind of book that's being shelved into respective areas 
+category_Count = {
+	'gen_stacks': 0,
+	'juv': 0,
+	'ref': 0,
+	'film': 0,
+	'magazines': 0,
+	'videos': 0,
+}
+
+print('Category Count Object: ', category_Count);
