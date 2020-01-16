fd2e94fd74c05f40c98fdd1ef9daa710bcb361fd
Akshay199456 <akshay.kum94@gmail.com>
2020-01-16 09:12:39 -0600

    Setup server successfully.

--- code.py
+++ code.py
@@ -1,3 +1,32 @@
+# Importing 'Image' from the 'Pillow' library and the 'pytesseract' library
+try:
+	from PIL import Image
+except ImportError:
+	import Image
+
+import pytesseract
+
+
+
+def ocr_core(filename):
+	'''
+	This function will handle the the core OCR processing of images and returns
+	the text stored in the image
+	'''
+
+	# Using Pillow's Image class to open the image and pytesseract to detect 
+	# the string in the image
+	text = pytesseract.image_to_string(Image.open(filename));
+	return text;
+
+print(ocr_core('images/google.png'))
+
+
+
+
+
+
+
 # Keep count of each kind of book that's being shelved into respective areas 
 category_Count = {
 	'gen_stacks': 0,
